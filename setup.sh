b221e8785e67bebd54a029ee4a4c741e4a80ae89
Tyler Ware <tyler.ware.777@gmail.com>
2017-12-28 11:16:08 -0700

    adding make directory for nvim config

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 rm ~/.vimrc
 ln -s  $cfg/vim/init.vim ~/.vimrc
 
+mkdir -p ~/.config/nvim
 rm ~/.config/nvim/init.vim
 ln -s  $cfg/vim/init.vim ~/.config/nvim/init.vim
 
+
+nvim -c "PlugInstall"
+nvim -c "UpdateRemotePlugins"
